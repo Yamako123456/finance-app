b017f423223dad1e6ca055295df128c01b26a38e
miyoko yamakawa <yamakodev@gmail.com>
2025-12-03 07:23:09 -0800

    Added props to pass individual data to each Card

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import "./Card.css";
 
-type Props = {};
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number
+};
 
-const Card = (props: Props) => {
+const Card = ({ companyName, ticker, price }: Props) => {
   return <div className="card">
     <img
       src='https://plus.unsplash.com/premium_photo-1680404114169-e254afa55a16?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       alt='image'
     />
     <div className='details'>
-      <h2>AAPL</h2>
-      <p>$110</p>
+      <h2>{companyName} ({ticker})</h2>
+      <p>{price}</p>
 
     </div>
     <p className='info'>
